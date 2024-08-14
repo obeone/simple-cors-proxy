556ebab2b036a34f862b55974a4bcee054802384
Grégoire Compagnon <obeone@obeone.org>
2024-08-14 05:36:17 +0200

    refactor(server): enhance middleware documentation and streamline header management
    Improved the documentation for middleware functions to provide clearer explanations of their roles and behaviors. Streamlined the process of removing specific headers in proxy requests by using array iteration. Enhanced logging for better traceability and readability. This refactor aims to improve code maintainability and developer understanding without altering the core functionality.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/Users/obeone/Documents/geek/github/simple-cors-proxy/server.js
+
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const bodyParser = require('body-parser');
@@ -6,17 +8,18 @@ const chalk = require('chalk'); // Module to add colors to console output
 
 const app = express();
 
-// Parse application/json content-type
+// Parse application/json content-type for incoming requests
 app.use(bodyParser.json());
 
-// Use morgan for logging incoming requests with added color for better readability
+// Use morgan middleware for logging incoming requests with colors for better readability
 app.use(morgan(chalk.blue(':method') + ' ' + chalk.green(':url') + ' ' + chalk.yellow(':status') + ' ' + chalk.magenta(':response-time ms')));
 
-// Middleware to parse URL parameters and add them to headers
+/**
+ * Middleware to parse URL query parameters and add them as headers to the request.
+ * It excludes the special 'url' and 'token' query parameters from being added to headers.
+ */
 const urlParamsToHeadersMiddleware = (req, res, next) => {
-    // Iterate over query parameters and add them to headers
     for (const [key, value] of Object.entries(req.query)) {
-        // Skip the special 'url' and 'token' query parameters
         if (key.toLowerCase() !== 'url' && key.toLowerCase() !== 'token') {
             req.headers[key.toLowerCase()] = value;
         }
@@ -24,7 +27,10 @@ const urlParamsToHeadersMiddleware = (req, res, next) => {
     next();
 };
 
-// Middleware to delete headers from the request
+/**
+ * Middleware to delete specified headers from the request.
+ * It checks for headers-to-delete specified in the request itself, query parameters, or environment variables.
+ */
 const deleteRequestHeadersMiddleware = (req, res, next) => {
     'use strict';
 
@@ -45,7 +51,10 @@ const deleteRequestHeadersMiddleware = (req, res, next) => {
     next();
 };
 
-// Middleware to delete headers from the response
+/**
+ * Middleware to delete specified headers from the response once it finishes.
+ * It checks for headers-to-delete-response specified in the response itself or environment variables.
+ */
 const deleteResponseHeadersMiddleware = (req, res, next) => {
     res.on('finish', () => {
         const responseHeadersToDelete = new Set([
@@ -60,102 +69,94 @@ const deleteResponseHeadersMiddleware = (req, res, next) => {
     next();
 };
 
-// Middleware to check an optional API key
+/**
+ * Middleware to check an optional API key in the request.
+ * It looks for an API key (`token`) in query parameters or headers and compares it with the expected token.
+ */
 const checkApiKeyMiddleware = (req, res, next) => {
     'use strict';
 
     const token = req.query.token || req.headers['x-proxy-token'];
     if (process.env.PROXY_TOKEN && token !== process.env.PROXY_TOKEN) {
-        res.sendStatus(401);
+        res.sendStatus(401); // Unauthorized
         return;
     }
 
     next();
 }
 
-// Configuration for the proxy middleware
+/**
+ * Configuration for the proxy middleware, defining behavior when proxying requests.
+ * Handles dynamic target URL determination and modifying both requests and responses.
+ */
 const corsProxyOptions = {
-    target: 'http://host_to_be_superseeded_by_router', // The target host (replaced by the X-Url-Destination header in router)
+    target: 'http://host_to_be_superseeded_by_router',
     changeOrigin: true,
     logLevel: 'debug', // Enable verbose logging for the proxy
     router: (req) => {
-        // Check if the request has a specific destination URL
-        const url = req.query.url || req.headers['x-url-destination'];
+        const url = req.query.url || req.headers['x-url-destination']; // Determine the destination URL
         if (url) {
             console.debug(chalk.cyan('Proxying request to host :'), chalk.cyanBright(new URL(url).origin));
             return new URL(url).origin;
-        }
-        else {
-            // Log and throw an error if the URL is not found
+        } else {
             console.debug(chalk.red('No URL found in query parameter or X-Url-Destination header'));
             throw new Error('You need to provide the URL as a query parameter or set the X-url-destination header');
         }
     },
-    pathRewrite: function (path, req) { 
-        // Take the full URL in req.query.url or req['x-url-destination'], and return only the path part
-        const url = req.query.url || req.headers['x-url-destination'];
+    pathRewrite: (path, req) => { 
+        const url = req.query.url || req.headers['x-url-destination']; // Determine the full destination URL
         console.debug(chalk.cyan('Proxying request to path :'), chalk.cyanBright(new URL(url).pathname + new URL(url).search));
         return new URL(url).pathname + new URL(url).search;
     },
     onProxyReq: (proxyReq, req, res) => {
-        // Log the proxying of the request and the original request headers
         console.debug(chalk.cyan('Proxying request to:'), chalk.cyanBright(req.url));
         console.debug(chalk.cyan('Original request headers:'), req.headers);
 
-        // Remove specific headers from the proxy request
-        proxyReq.removeHeader('x-forwarded-host');
-        proxyReq.removeHeader('x-forwarded-proto');
-        proxyReq.removeHeader('x-forwarded-for');
-        proxyReq.removeHeader('x-url-destination');
+        // Remove specific headers from being forwarded by the proxy
+        ['x-forwarded-host', 'x-forwarded-proto', 'x-forwarded-for', 'x-url-destination'].forEach(header => proxyReq.removeHeader(header));
         
-        // Log the modified request headers
         console.debug(chalk.cyan('Modified request headers:'), proxyReq.getHeaders());
     },
     onProxyRes: (proxyRes, req, res) => {
-        // Log the received response status and original response headers
         console.debug(chalk.green('Received response with status:'), chalk.greenBright(proxyRes.statusCode));
         console.debug(chalk.green('Original response headers:'), proxyRes.headers);
 
-        // Adjust response headers based on the original request
+        // Add CORS headers to the response
         proxyRes.headers['Access-Control-Allow-Origin'] = req.headers['origin'] || '*';
         proxyRes.headers['Access-Control-Allow-Methods'] = req.headers['access-control-request-method'] || 'GET,POST,PUT,PATCH,DELETE,OPTIONS';
         proxyRes.headers['Access-Control-Allow-Headers'] = req.headers['access-control-request-headers'] || 'Origin, Content-Type, Accept, Authorization';
         
-        // Log the modified response headers
         console.debug(chalk.green('Modified response headers:'), proxyRes.headers);
     },
     onError: (err, req, res) => {
-        // Log any errors encountered by the proxy
         console.error(chalk.red('Proxy encountered an error:'), err);
     },
 };
 
-// Handle OPTIONS requests directly with user-friendly logging
+// Handle OPTIONS requests for CORS preflight checks with user-friendly logging
 app.options('/proxy', (req, res) => {
     console.log(chalk.yellow('Received OPTIONS request'));
     res.header('Access-Control-Allow-Origin', req.headers['origin'] || '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers'] || 'Origin, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Max-Age', '86400'); // 24 hours
+    res.header('Access-Control-Max-Age', '86400'); // 24 hours cache duration
     res.sendStatus(200);
 });
 
-// Apply middleware to handle query parameters for headers
+// Register middlewares for query parameter handling, API key checking, and proxying
 app.use('/proxy', urlParamsToHeadersMiddleware);
-
-// Apply middleware to check an optional API key
 app.use(checkApiKeyMiddleware);
 
-// Apply the CORS proxy middleware to the path '/proxy'
+// Setup the proxy middleware
 app.use('/proxy', createProxyMiddleware(corsProxyOptions));
 
-// Apply the middleware to delete request headers
+// Apply middlewares to delete headers from request and response
 app.use(deleteRequestHeadersMiddleware);
-// Apply the middleware to delete response headers
 app.use(deleteResponseHeadersMiddleware);
 
-// Start the server with user-friendly logging
+// Start the server with a success message logging
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(chalk.green(`Server is running on port ${PORT}`));
 });
+
